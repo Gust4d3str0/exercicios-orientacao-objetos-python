8f4cd08b577da896738bd3d68d3e69e00966f052
Gustavo Femiano <henriquefemiano@outlook.com>
2025-02-27 16:00:12 -0300

    adicionando nova bebida

--- OOSABOREXPRESS/app.py
+++ OOSABOREXPRESS/app.py
@@ -4,6 +4,7 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 5.00, 'Grande')
+bebida_vinho = Bebida('vinho', 15.00, 'taca')
 bebida_suco.aplicar_desconto
 prato_paozinho = Prato('Paozinho', 2.00, 'Pao') 
 prato_paozinho.aplicar_desconto()
